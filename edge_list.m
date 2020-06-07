c4ac74064a19fd102e5e526f124ffc6bd608cf48
henrikforberg <54591385+henrikforberg@users.noreply.github.com>
2020-06-07 15:04:40 +0200

    Update edge_list.m

--- edge_list.m
+++ edge_list.m
@@ -1,5 +1,4 @@
-function[EdgeList] = edge_list(E, num)
-
+function [EdgeList] = edge_list(E, num)
     % Return the edgelist for matrix E
     
     EdgeList = [];
@@ -22,8 +21,6 @@ function[EdgeList] = edge_list(E, num)
            temp(count) = E(1, B(k));
            count = count+1;
        end
-       %jfk = EdgeList
-       %mistersir = temp
        EdgeList = [EdgeList temp];
     end
 end
